24a61105db43cb06e253b2e413ab4a612c8ea468
Karina Tae <karin.rifa@gmail.com>
2019-10-25 16:10:07 +0700

    inisialisasi2

--- app/src/main/java/com/example/uts/MainActivity.java
+++ app/src/main/java/com/example/uts/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.uts;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,4 +12,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    new Handler().postDelayed(new Runnable() {@Override public void run (){
+        Intent intent=onNewIntent(MainActivity.this,activity_register.xml.class);
+        startActivity(intent);finish();}},waktu_loading(3000);
+    })
+
 }
